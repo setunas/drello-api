d05eb50664a40503548f07d1a9b412f4433a68e2
nao-noah <naomooo.official@gmail.com>
2021-09-07 17:49:46 -0700

    Set parameters on Open function

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"drello-api/pkg/infrastructure/mysql"
 	"drello-api/pkg/presentation/rest"
+	"drello-api/pkg/utils"
 	"log"
 )
 
 func main() {
-	err := mysql.Open()
+	var (
+		dbUser    = utils.MustGetenv("DB_USER")     // e.g. 'my-db-user'
+		dbPwd     = utils.MustGetenv("DB_PASS")     // e.g. 'my-db-password'
+		dbTCPHost = utils.MustGetenv("DB_TCP_HOST") // e.g. '127.0.0.1'
+		dbPort    = utils.MustGetenv("DB_PORT")     // e.g. '3306'
+		dbName    = utils.MustGetenv("DB_NAME")     // e.g. 'my-database'
+	)
+
+	err := mysql.Open(dbUser, dbPwd, dbTCPHost, dbPort, dbName)
 	if err != nil {
 		log.Println(err)
 	}
