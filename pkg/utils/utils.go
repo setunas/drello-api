449827ef95dfbb2e1c1883387dee063375d484b6
nao-noah <naomooo.official@gmail.com>
2021-09-07 17:48:58 -0700

    Add function to make sure to set the env

---
+++ pkg/utils/utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"log"
+	"os"
+)
+
+func MustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("Warning: %s environment variable not set.\n", k)
+	}
+	return v
+}
