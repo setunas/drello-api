a459e14c2ceb95ab456c602e19499da8a343e571
nao-noah <naomooo.official@gmail.com>
2021-11-02 14:13:27 -0700

    Use existing function to open db connection

--- pkg/presentation/rest/main_test.go
+++ pkg/presentation/rest/main_test.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"drello-api/pkg/infrastructure/mysql"
+	"drello-api/pkg/utils"
 	"log"
 	"os"
 	"testing"
@@ -10,7 +11,15 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	err := mysql.TestOpen()
+	var (
+		dbUser    = utils.MustGetenv("TEST_DB_USER")     // e.g. 'my-db-user'
+		dbPwd     = utils.MustGetenv("TEST_DB_PASS")     // e.g. 'my-db-password'
+		dbTCPHost = utils.MustGetenv("TEST_DB_TCP_HOST") // e.g. '127.0.0.1'
+		dbPort    = utils.MustGetenv("TEST_DB_PORT")     // e.g. '3306'
+		dbName    = utils.MustGetenv("TEST_DB_NAME")     // e.g. 'my-database'
+	)
+
+	err := mysql.Open(dbUser, dbPwd, dbTCPHost, dbPort, dbName)
 	if err != nil {
 		log.Println(err)
 	}
