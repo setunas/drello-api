90877305558bd5d5f4f952f09a5eb0a304b252c6
nao-noah <naomooo.official@gmail.com>
2021-12-22 21:05:46 -0800

    Add body to logging

--- pkg/presentation/rest/logging.go
+++ pkg/presentation/rest/logging.go
@@ -1,17 +1,22 @@
 package rest
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func logHTTPRequest(r *http.Request) {
+	var body interface{}
+	json.NewDecoder(r.Body).Decode(&body)
+
 	requestLog := map[string]string{
 		"Method":    r.Method,
 		"URI":       r.URL.String(),
 		"Referer":   r.Header.Get("Referer"),
 		"UserAgent": r.Header.Get("User-Agent"),
+		"Body":      fmt.Sprintln(body),
 	}
 
 	message := "HTTP Request:"
