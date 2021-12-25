982dcf32d169f873e8a040129a1463f4defdd71a
nao-noah <naomooo.official@gmail.com>
2021-12-24 18:38:41 -0800

    Add create for cards

--- pkg/app/cards/create.go
+++ pkg/app/cards/create.go
@@ -4,9 +4,22 @@ import (
 	"context"
 	"drello-api/pkg/app/repository"
 	"drello-api/pkg/domain/card"
+	"fmt"
 )
 
-func Create(ctx context.Context, cardRepo repository.Card, input *CreateInput) (*CreateOutput, error) {
+func Create(ctx context.Context, columnRepo repository.Column, cardRepo repository.Card, userRepo repository.User, input *CreateInput) (*CreateOutput, error) {
+	user, err := userRepo.GetOneByFirebaseUID(ctx, input.firebaseUID)
+	if err != nil {
+		return nil, err
+	}
+	column, err := columnRepo.GetOneById(ctx, input.columnId)
+	if err != nil {
+		return nil, err
+	}
+	if user.BoardID() != column.BoardId() {
+		return nil, fmt.Errorf("invalid board ID: %d, user's borad ID is: %d", column.BoardId(), user.BoardID())
+	}
+
 	cardDomain, err := cardRepo.Create(ctx, input.title, input.description, input.columnId)
 	if err != nil {
 		return nil, err
@@ -19,10 +32,11 @@ type CreateInput struct {
 	title       string
 	description string
 	columnId    int
+	firebaseUID string
 }
 
-func NewCreateInput(title string, description string, columnId int) *CreateInput {
-	return &CreateInput{title: title, description: description, columnId: columnId}
+func NewCreateInput(title string, description string, columnId int, firebaseUID string) *CreateInput {
+	return &CreateInput{title: title, description: description, columnId: columnId, firebaseUID: firebaseUID}
 }
 
 type CreateOutput struct {
