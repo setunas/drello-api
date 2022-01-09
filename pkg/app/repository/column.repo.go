4e90cb1ffd4373022b36603a03abe5d31ea7c4b4
nao-noah <naomooo.official@gmail.com>
2022-01-08 16:43:24 -0800

    Add positon to repo

--- pkg/app/repository/column.repo.go
+++ pkg/app/repository/column.repo.go
@@ -8,7 +8,7 @@ import (
 type Column interface {
 	GetOneByID(ctx context.Context, columnID int) (*column.Column, error)
 	GetListByBoardId(ctx context.Context, boardId int) (*[]*column.Column, error)
-	Create(ctx context.Context, title string, boardId int) (*column.Column, error)
-	Update(ctx context.Context, id int, title string, boardId int) (*column.Column, error)
+	Create(ctx context.Context, title string, position float64, boardId int) (*column.Column, error)
+	Update(ctx context.Context, id int, title string, position float64, boardId int) (*column.Column, error)
 	Delete(ctx context.Context, id int) error
 }
