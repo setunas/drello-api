e38c78d893730c97e0e3a4cb3060fd290b48a113
nao-noah <naomooo.official@gmail.com>
2022-01-08 16:40:31 -0800

    Add postion to domain

--- pkg/domain/column/column.go
+++ pkg/domain/column/column.go
@@ -1,13 +1,14 @@
 package column
 
 type Column struct {
-	id      int
-	title   string
-	boardId int
+	id       int
+	title    string
+	position float64
+	boardId  int
 }
 
-func New(id int, title string, boardId int) *Column {
-	return &Column{id: id, title: title, boardId: boardId}
+func New(id int, title string, position float64, boardId int) *Column {
+	return &Column{id: id, title: title, position: position, boardId: boardId}
 }
 
 func (c *Column) ID() int {
@@ -16,6 +17,10 @@ func (c *Column) ID() int {
 
 func (c *Column) Title() string {
 	return c.title
+}
+
+func (c *Column) Positon() float64 {
+	return c.position
 }
 
 func (c *Column) BoardId() int {
