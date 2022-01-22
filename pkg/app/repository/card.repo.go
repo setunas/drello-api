b7a64862ee1fafd2414b1b7214b7d5d37da953ff
nao-noah <naomooo.official@gmail.com>
2022-01-22 12:48:42 -0800

    Add getListByIds to repo

--- pkg/app/repository/card.repo.go
+++ pkg/app/repository/card.repo.go
@@ -7,6 +7,7 @@ import (
 
 type Card interface {
 	GetOneByID(ctx context.Context, id int) (*card.Card, error)
+	GetListByIDs(ctx context.Context, ids []int) (*[]*card.Card, error)
 	GetListByColumnIds(ctx context.Context, columnIds []int) (*[]*card.Card, error)
 	Create(ctx context.Context, title string, description string, position float64, columnId int) (*card.Card, error)
 	Update(ctx context.Context, id int, title string, description string, position float64, columnId int) (*card.Card, error)
