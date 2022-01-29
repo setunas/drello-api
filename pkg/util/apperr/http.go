770fbc580094e213fac24937e320921a4dece537
nao-noah <naomooo.official@gmail.com>
2022-01-29 00:37:34 -0800

    Move common code to common file

--- pkg/util/apperr/http.go
+++ pkg/util/apperr/http.go
@@ -1,10 +1,5 @@
 package apperr
 
-import (
-	"fmt"
-	"runtime"
-)
-
 type HTTPError struct {
 	status     int
 	message    string
@@ -56,24 +51,6 @@ func inheritStatus(newStatus int, oldStatus int) int {
 	} else {
 		return newStatus
 	}
-}
-
-func combineMessages(message string, err error) string {
-	if message != "" && err != nil {
-		return message + ": " + err.Error()
-	} else if err != nil {
-		return err.Error()
-	} else {
-		return message
-	}
-}
-
-func newOccurredAt() string {
-	pc := make([]uintptr, 10)
-	runtime.Callers(3, pc)
-	f := runtime.FuncForPC(pc[0])
-	file, line := f.FileLine(pc[0])
-	return fmt.Sprintf("%s:%d %s", file, line, f.Name())
 }
 
 var tagStatusMap = map[Tag]int{
