cb62631cb166471db689ed33fb2cf055ef896054
nao-noah <naomooo.official@gmail.com>
2022-01-28 17:43:23 -0800

    Use AuthenticateUser in boards.patch

--- pkg/app/usecase/updateBoard/updateBoard.go
+++ pkg/app/usecase/updateBoard/updateBoard.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"drello-api/pkg/app/repository"
 	"drello-api/pkg/domain/board"
+	"drello-api/pkg/domain/user"
 	"fmt"
 )
 
-func Call(ctx context.Context, boardID int, title string, firebaseUID string) (*board.Board, error) {
-	err := authorize(ctx, firebaseUID, boardID)
+func Call(ctx context.Context, boardID int, title string, user *user.User) (*board.Board, error) {
+	err := authorize(ctx, user, boardID)
 	if err != nil {
 		return nil, err
 	}
@@ -21,12 +22,7 @@ func Call(ctx context.Context, boardID int, title string, firebaseUID string) (*
 	return boardDomain, nil
 }
 
-func authorize(ctx context.Context, firebaseUID string, boardID int) error {
-	user, err := (*repository.UserDS()).GetOneByFirebaseUID(ctx, firebaseUID)
-	if err != nil {
-		return err
-	}
-
+func authorize(ctx context.Context, user *user.User, boardID int) error {
 	if user.BoardID() != boardID {
 		return fmt.Errorf("invalid board ID: %d, user's borad ID is: %d", boardID, user.BoardID())
 	}
