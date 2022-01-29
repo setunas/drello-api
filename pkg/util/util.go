d07276f32b9f92b9352df3a237cf8919cd658f6a
nao-noah <naomooo.official@gmail.com>
2022-01-28 23:47:32 -0800

    Use format type

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -8,7 +8,7 @@ import (
 func MustGetenv(k string) string {
 	v := os.Getenv(k)
 	if v == "" {
-		log.Warn("%s environment variable not set.", k).Write()
+		log.Warnf("%s environment variable not set.", k).Write()
 	}
 	return v
 }
