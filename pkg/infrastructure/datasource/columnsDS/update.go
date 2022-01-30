a0aa8b72d2cbd1a865cbf1349c2bf36b8f9013d5
nao-noah <naomooo.official@gmail.com>
2022-01-30 10:14:31 -0800

    Update logs

--- pkg/infrastructure/datasource/columnsDS/update.go
+++ pkg/infrastructure/datasource/columnsDS/update.go
@@ -12,7 +12,7 @@ import (
 func (c ColumnsDS) Update(ctx context.Context, id int, title string, position float64, boardId int) (*domainColumn.Column, error) {
 	db := mysql.DBPool()
 	query := "UPDATE columns SET title = ?, position = ? WHERE id = ?"
-	log.Info("SQL").Add("RequestID", restutil.RetrieveReqID(ctx)).Add("SQL", query).Add("query", query).Write()
+	log.Info("SQL").Add("RequestID", restutil.RetrieveReqID(ctx)).Add("SQL", query).Add("title", title).Add("position", position).Write()
 	_, err := db.Exec(query, title, position, id)
 	if err != nil {
 		return nil, fmt.Errorf("failed updating column: %w", err)
